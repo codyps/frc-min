ed3cc546d18147c1ca16ce490fa2338686a898ac
Cody P Schafer <jmesmon@gmail.com>
2013-02-17 00:00:22 -0800

    replace module-from-symbol linear lookup with hashtable using private functionality.

--- vxworks-util.c
+++ vxworks-util.c
@@ -3,54 +3,31 @@
 
 #include "vxworks-util.h"
 
-#include <sysSymTbl.h> /* sysSymTbl */
-#include <moduleLib.h> /* SEGMENT_ID, moduleSegFirst(), moduleSegNext() */
+#include <private/symLibP.h>
+#include <private/symbolP.h>
+#include <sysSymTbl.h>
 
-static bool segment_contains_addr(SEGMENT_ID seg, void *addr)
+/* Based on documentation in vxworks-6.3/target/h/private/symbolP.h */
+static MODULE_ID symref_is_module_or_null(uint32_t symref)
 {
-	return seg->address <= addr && addr < (seg->address + seg->size);
-}
-
-/* XXX: confirm that the termination check is correct. */
-#define for_each_segment_in_module(segment_cursor, module)	\
-	for (segment_cursor = moduleSegFirst(module);		\
-	     segment_cursor;					\
-	     segment_cursor = moduleSegNext(segment_cursor))
-
-static bool module_contains_addr(MODULE_ID mod, void *addr)
-{
-	SEGMENT_ID seg;
-	for_each_segment_in_module(seg, mod) {
-		if (segment_contains_addr(seg, addr))
-			return true;
+	switch (symref) {
+	case (uint32_t)SYMREF_SHELL:
+	case (uint32_t)SYMREF_KERNEL:
+	case (uint32_t)SYMREF_WTX:
+		return NULL;
+	default:
+		return (MODULE_ID)symref;
 	}
-
-	return false;
 }
 
-static BOOL on_each_module(MODULE_ID mod, int addr)
+/* This uses symFindSymbol(), from vxworks-6.3/target/h/private/symLibP.h
+ * - uses a single hash table lookup to find the module. */
+MODULE_ID module_find_by_symbolname(const char *sym_name)
 {
-	if (module_contains_addr(mod, (void *)(intptr_t)addr)) {
-		return FALSE;
-	}
-	return TRUE;
-}
-
-static MODULE_ID module_find_by_addr(void *addr)
-{
-	/* XXX: kernelModuleListId _appears_ to be a global that would allow us
-	 * to do for_XXX loop over the DL_LIST here. Unfortunately, it is
-	 * "private". */
-	return moduleEach((FUNCPTR)on_each_module, (int)addr);
-}
-
-MODULE_ID module_find_by_symbolname(const char *sym)
-{
-	char *val;
-	SYM_TYPE type;
-	STATUS r = symFindByName(sysSymTbl, (char *)sym, &val, &type);
-	if (r == ERROR)
+	SYMBOL_ID sym;
+	/* when 'name' is passed, the 'value', 'type', and 'mask' are ignored */
+	if (symFindSymbol(sysSymTbl, (char *)sym_name, 0, 0, 0, &sym) == ERROR)
 		return NULL;
 
-	return module_find_by_addr(val);
+	return symref_is_module_or_null(sym->symRef);
 }
