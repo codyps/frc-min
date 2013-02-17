98904d4aff95364f9e97236169ac6370da10abc1
Cody P Schafer <jmesmon@gmail.com>
2013-02-16 17:21:24 -0800

    doc changes.

--- frc-drop-in.h
+++ frc-drop-in.h
@@ -3,10 +3,13 @@
 
 #include "vxworks-util.h"
 
-/* XXX: moduleNameFIndBySymbolName() is provided by the
+/* XXX: moduleNameFIndBySymbolName() is defined in the header
  * WPILib/NetworkCommunication/symModuleLink.h
  *
- * Most users should use module_find_by_symbolname() instead
+ * And probably provided in the FRC Networking Library (or something).
+ * Basically: we have no source code.
+ *
+ * Users should use module_find_by_symbolname() instead.
  */
 static inline STATUS moduleNameFindBySymbolName(const char *sym, char *module)
 {
--- main.c
+++ main.c
@@ -26,7 +26,6 @@ static void unload_old_module(void)
 	unldByModuleId(old_module_id, 0);
 }
 
-
 extern int32_t INIT_FUNC(void);
 
 /* Called by the code that loads this module (FRC_UserProgram.out) */
@@ -37,5 +36,19 @@ int32_t INIT_FUNC(void)
 	/* TODO: establish net comms. */
 
 	/* TODO: spawn some task(s) */
+#if 0
+	/* XXX: note that WPILib documentation indicate a new task is spawned
+	 * to ensure the task running user code has the VX_FP_TASK option set.
+	 */
+	int32_t task_id = taskSpawn(TASK_NAME,
+				    101 /* default priority */,
+				    VX_FP_TASK, /* options */
+				    64000, /* stack size */
+				    my_func, /* function to start */
+				    0,0,0,0,0,0,0,0,0,0 /* 10 uint32_t arguments */
+				    /* In C++: arg0 = FRC_userClassFactory,
+				     * arg1 = (Task *) */
+				   );
+#endif
 	return 0;
 }
